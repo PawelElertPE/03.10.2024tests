0f6b99f62a7885875b1ae28afcfc627601943fb8
Pawel Elert <pawelelert@wp.pl>
2024-10-03 10:59:37 +0200

    python.py has been updated

--- python.py
+++ python.py
@@ -3,6 +3,9 @@ import time
 i = 0
 while True:
     print("keep coding in python")
+    start_time = time.time()
     time.sleep(3)
+    end_time = time.time()
     i += 1
     print(f"This loop has been executed {i} times")
+    print(f"It took {end_time - start_time} to perform the loop once")
